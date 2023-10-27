cb9ba86edd50ea7e6f5c8769d31096ff5d58661a
doloresgarcia <doloresg382@gmail.com>
2023-10-27 14:18:16 +0200

    debug

--- pointcept/models/UD_pulling/tools.py
+++ pointcept/models/UD_pulling/tools.py
@@ -16,82 +16,6 @@ import torch_cmspepr
 """
 
 
-def src_dot_dst(src_field, dst_field, out_field):
-    def func(edges):
-        return {
-            out_field: (edges.src[src_field] * edges.dst[dst_field]).sum(
-                -1, keepdim=True
-            )
-        }
-
-    return func
-
-
-def scaled_exp(field, scale_constant):
-    def func(edges):
-        # clamp for softmax numerical stability
-        return {field: torch.exp((edges.data[field] / scale_constant).clamp(-5, 5))}
-
-    return func
-
-
-"""
-    Single Attention Head
-"""
-
-
-class MultiHeadAttentionLayer(nn.Module):
-    def __init__(self, in_dim, out_dim, num_heads, use_bias):
-        super().__init__()
-
-        self.out_dim = out_dim
-        self.num_heads = num_heads
-
-        if use_bias:
-            self.Q = nn.Linear(in_dim, out_dim * num_heads, bias=True)
-            self.K = nn.Linear(in_dim, out_dim * num_heads, bias=True)
-            self.V = nn.Linear(in_dim, out_dim * num_heads, bias=True)
-        else:
-            self.Q = nn.Linear(in_dim, out_dim * num_heads, bias=False)
-            self.K = nn.Linear(in_dim, out_dim * num_heads, bias=False)
-            self.V = nn.Linear(in_dim, out_dim * num_heads, bias=False)
-
-    def propagate_attention(self, g):
-        # Compute attention score
-
-        g.apply_edges(src_dot_dst("K_h", "Q_h", "score"))  # , edges)
-
-        g.apply_edges(scaled_exp("score", np.sqrt(self.out_dim)))
-        # Send weighted values to target nodes
-        eids = g.edges()
-        print(g.ndata["V_h"].shape, g.edata["score"].shape)
-        g.send_and_recv(
-            eids,
-            fn.u_mul_e("V_h", "score", "V_h"),
-            fn.sum("V_h", "wV"),  # deprecated in dgl 1.0.1
-        )
-
-        g.send_and_recv(
-            eids, fn.copy_e("score", "score"), fn.sum("score", "z")
-        )  # copy_e deprecated in dgl 1.0.1
-
-    def forward(self, g, h):
-
-        Q_h = self.Q(h)
-        K_h = self.K(h)
-        V_h = self.V(h)
-
-        # Reshaping into [num_nodes, num_heads, feat_dim] to
-        # get projections for multi-head attention
-        g.ndata["Q_h"] = Q_h.view(-1, self.num_heads, self.out_dim)
-        g.ndata["K_h"] = K_h.view(-1, self.num_heads, self.out_dim)
-        g.ndata["V_h"] = V_h.view(-1, self.num_heads, self.out_dim)
-        self.propagate_attention(g)
-        head_out = g.ndata["wV"] / g.ndata["z"]
-
-        return head_out
-
-
 def knn_per_graph(g, sl, k):
     graphs_list = dgl.unbatch(g)
     node_counter = 0
@@ -212,11 +136,12 @@ class MultiHeadAttentionLayer(nn.Module):
     def propagate_attention(self, g):
         # Compute attention score
         g.apply_edges(src_dot_dst("K_h", "Q_h", "score"))  # , edges)
-        # print(g.edata["score"])
+        print(g.edata["score"].shape)
         g.apply_edges(scaled_exp("score", np.sqrt(self.out_dim)))
-        # print(g.edata["score"], "Score 2")
+        print(g.edata["score"].shape, "Score 2")
         # Send weighted values to target nodes
         eids = g.edges()
+        print(g.ndata["V_h"].shape)
         g.send_and_recv(
             eids,
             fn.u_mul_e("V_h", "score", "V_h"),

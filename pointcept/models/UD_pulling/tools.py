7fe1cab7265065eff81cbcb21aff4fdd2283101d
doloresgarcia <doloresg382@gmail.com>
2023-10-27 15:02:25 +0200

    allow random coords

--- pointcept/models/UD_pulling/tools.py
+++ pointcept/models/UD_pulling/tools.py
@@ -312,7 +312,7 @@ class Swin3D(nn.Module):
         self.find_up = FindUpPoints()
         self.embedding_scores = nn.Linear(in_dim_node, 1)  # node feat is an integer
         self.sigmoid_scores = nn.Sigmoid()
-        self.funky_coordinate_space = False
+        self.funky_coordinate_space = True
         if self.funky_coordinate_space:
             self.embedding_coordinates = nn.Linear(
                 in_dim_node, 3

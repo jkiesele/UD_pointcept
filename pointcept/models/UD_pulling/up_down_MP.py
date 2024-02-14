b7bd0b3a8c2487a139cf836184e5fcba641f3e38
doloresgarcia <doloresg382@gmail.com>
2024-02-14 16:09:42 +0100

    refactor

---
+++ pointcept/models/UD_pulling/up_down_MP.py
@@ -0,0 +1,191 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import dgl
+import dgl.function as fn
+import sys
+from functools import partial
+import os.path as osp
+import time
+import numpy as np
+import torch_cmspepr
+from torch import Tensor
+from torch.nn import Linear
+
+
+class MLP_difs_softmax(nn.Module):
+    """
+    Param:
+    """
+
+    def __init__(
+        self,
+        in_dim,
+        out_dim,
+    ):
+        super().__init__()
+
+        self.in_channels = in_dim
+        self.out_channels = out_dim
+        self.edgedistancespassing_softmax = EdgeDistancesPassing_softmax(
+            self.in_channels, self.out_channels
+        )
+        self.edgedistancespassing_1 = EdgeDistancesPassing_1()
+
+        self.norm = EdgeWeightNorm(norm="right")
+        self.meanmaxaggregation = MeanMax_aggregation(self.out_channels)
+        # self.batch_norm = nn.BatchNorm1d(out_dim)
+        # self.FFN_layer1 = nn.Linear(out_dim, out_dim * 2)
+        # self.FFN_layer2 = nn.Linear(out_dim * 2, out_dim)
+        # self.batch_norm2 = nn.BatchNorm1d(out_dim)
+
+    def forward(self, g, h):
+        h_in = h
+        g.ndata["features"] = h
+        g.apply_edges(self.edgedistancespassing)
+        norm_edge_weight = self.norm(
+            dgl.reverse(g), g.edata["att_weight"]
+        )  # this allows to spread the info of one node into upnodes
+        g.edata["att_weight"] = norm_edge_weight
+        g.update_all(self.edgedistancespassing_1, fn.sum("feature_n", "h_updated"))
+        h = g.ndata["h_updated"]
+        # h = self.batch_norm(h)
+        # h_in2 = h
+        # h = self.FFN_layer1(h)
+        # h = F.relu(h)
+        # h = self.FFN_layer2(h)
+        # h = h_in2 + h  # residual connection
+        # h = self.batch_norm2(h)
+        return h
+
+
+class EdgeDistancesPassing_softmax(nn.Module):
+    """
+    Compute the input feature from neighbors
+    """
+
+    def __init__(self, in_dim, out_dim):
+        super(EdgeDistancesPassing, self).__init__()
+        self.MLP = nn.Sequential(
+            nn.Linear(in_dim, out_dim),  #! Dense 3
+            nn.ReLU(),
+            nn.Linear(out_dim, 1),  #! Dense 4
+            nn.ReLU(),
+        )
+
+    def forward(self, edges):
+        dif = edges.src["features"] - edges.dst["features"]
+        att_weight = self.MLP(dif)
+        att_weight = torch.sigmoid(att_weight)  #! try sigmoid
+        return {"att_weight": att_weight}
+
+
+
+
+class MLP_difs(nn.Module):
+    """
+    Param:
+    """
+
+    def __init__(
+        self,
+        in_dim,
+        out_dim,
+    ):
+        super().__init__()
+
+        self.in_channels = in_dim
+        self.out_channels = out_dim
+        self.edgedistancespassing = EdgeDistancesPassing(
+            self.in_channels, self.out_channels
+        )
+        self.meanmaxaggregation = MeanMax_aggregation(self.out_channels)
+        self.batch_norm = nn.BatchNorm1d(out_dim)
+        self.FFN_layer1 = nn.Linear(out_dim, out_dim * 2)
+        self.FFN_layer2 = nn.Linear(out_dim * 2, out_dim)
+        self.batch_norm2 = nn.BatchNorm1d(out_dim)
+
+    def forward(self, g, h):
+        h_in = h
+        g.ndata["features"] = h
+        g.update_all(self.edgedistancespassing, self.meanmaxaggregation)
+        h = g.ndata["h_updated"]
+        h = h_in + h
+        h = self.batch_norm(h)
+        h_in2 = h
+        h = self.FFN_layer1(h)
+        h = F.relu(h)
+        h = self.FFN_layer2(h)
+        h = h_in2 + h  # residual connection
+        h = self.batch_norm2(h)
+        return h
+
+class EdgeDistancesPassing_1(nn.Module):
+    """
+    Compute the input feature from neighbors
+    """
+
+    def __init__(self):
+        super(EdgeDistancesPassing_1, self).__init__()
+
+    def forward(self, edges):
+        feature = edges.edata["att_weight"] * edges.src["features"]
+        return {"feature_n": feature}
+
+
+class EdgeDistancesPassing(nn.Module):
+    """
+    Compute the input feature from neighbors
+    """
+
+    def __init__(self, in_dim, out_dim):
+        super(EdgeDistancesPassing, self).__init__()
+        self.MLP = nn.Sequential(
+            nn.Linear(in_dim, out_dim),  #! Dense 3
+            nn.ReLU(),
+            nn.Linear(out_dim, 1),  #! Dense 4
+            nn.ReLU(),
+        )
+
+    def forward(self, edges):
+        dif = edges.src["features"] - edges.dst["features"]
+        att_weight = self.MLP(dif)
+        att_weight = torch.sigmoid(att_weight)  #! try sigmoid
+        feature = att_weight * edges.src["features"]
+        return {"feature_n": feature}
+
+
+class MeanMax_aggregation(nn.Module):
+    """
+    Feature aggregation in a DGL graph
+    """
+
+    def __init__(self, out_dim):
+        super(MeanMax_aggregation, self).__init__()
+        self.O = nn.Linear(out_dim * 2, out_dim)
+
+    def forward(self, nodes):
+        mean_agg = torch.mean(nodes.mailbox["feature_n"], dim=1)
+        max_agg = torch.max(nodes.mailbox["feature_n"], dim=1)[0]
+        out = torch.cat([mean_agg, max_agg], dim=-1)
+        out = self.O(out)
+        return {"h_updated": out}
+
+
+class MeanMax_aggregation_2(nn.Module):
+    """
+    Feature aggregation in a DGL graph
+    """
+
+    def __init__(self, out_dim):
+        super(MeanMax_aggregation_2, self).__init__()
+        self.O = nn.Linear(out_dim * 2, out_dim)
+
+    def forward(self, nodes):
+        mean_agg = torch.mean(nodes.mailbox["feature_n"], dim=1)
+        max_agg = torch.max(nodes.mailbox["feature_n"], dim=1)[0]
+        out = torch.cat([mean_agg, max_agg], dim=-1)
+        out = self.O(out)
+        return {"h_updated": out}
+

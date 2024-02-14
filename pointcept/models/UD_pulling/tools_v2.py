82abfc19b4622e4913df4ed16e9363b38450deaa
doloresgarcia <doloresg382@gmail.com>
2024-02-14 14:29:57 +0100

    db

--- pointcept/models/UD_pulling/tools_v2.py
+++ pointcept/models/UD_pulling/tools_v2.py
@@ -575,13 +575,7 @@ class Downsample(nn.Module):
             dist_to_up = torch.cdist(s_l_i[~up_points_i], s_l_i[up_points_i])
             # take the smallest distance and take first M
             # indices_connect = torch.topk(-dist_to_up, k=M_i, dim=1)[1]
-            dis_print = dist_to_up.clone()
-            dis_print = dis_print.detach().cpu().numpy()
-            with open("dis_print.pickle", "wb") as handle:
-                import pickle
 
-                pickle.dump(dis_print, handle)
-            0 / 0
             indices_connect = top_k_per_node(-dist_to_up, M_i)
             j = nodes_up[indices_connect]
             j = j.view(-1)

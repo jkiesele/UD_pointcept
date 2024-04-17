33128402c93d6d7069692800d104742011422767
doloresgarcia <doloresg382@gmail.com>
2024-04-17 18:09:49 +0200

    db

--- pointcept/models/UD_pulling/UP_pulling_v2.py
+++ pointcept/models/UD_pulling/UP_pulling_v2.py
@@ -168,6 +168,7 @@ class UNet(nn.Module):
         latest_depth_rep = []
         for l, (mp, down) in enumerate(zip(self.message_passing, self.contract_blocks)):
             # Do message passing flat and store features for skipped connections
+            print("MP", h.shape)
             g = mp(g, h, c)
             adj_m.append([g.edges[0], g.edges[1]])
             s_l = g.ndata["s_l"]
@@ -195,7 +196,8 @@ class UNet(nn.Module):
 
             h = self.push_info_up(h, h_above, idx, i, j)
             i, j = adj_m[up_idx - 1]
-            g = dgl.graph((i, j), num_nodes=h.shape[0])
+            print("MP up", h.shape)
+            g = dgl.graph((i, j), num_nodes=h_above.shape[0])
             g.ndata["h"] = h
             g = self.message_passing_up[i](g)
 
